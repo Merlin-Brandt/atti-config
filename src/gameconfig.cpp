0f2b1a3a5df3ce596ef87e83de961268468b1c04
Merlin Brandt <merlin.brandt@hotmail.de>
2022-02-02 11:32:24 +0100

    minor changes

--- src/gameconfig.cpp
+++ src/gameconfig.cpp
@@ -1,16 +1,3 @@
-/*
-    src/example1.cpp -- C++ version of an example application that shows
-    how to use the various widget classes. For a Python implementation, see
-    '../python/example1.py'.
-
-    NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
-    The widget drawing code is based on the NanoVG demo application
-    by Mikko Mononen.
-
-    All rights reserved. Use of this source code is governed by a
-    BSD-style license that can be found in the LICENSE.txt file.
-*/
-
 #include <GLFW/glfw3.h>
 #include <nanogui/screen.h>
 #include <nanogui/window.h>
@@ -95,9 +82,6 @@ public:
         tabs->set_callback([=] (int id) {tabs->set_selected_id(id);});
 
         Widget *tab = next_tab(tabs, "Giocatori");
-
-        /* No need to store a pointer, the data structure will be automatically
-           freed when the parent window is deleted */
 
         struct {
             TextBox *nome, *notizia;  
